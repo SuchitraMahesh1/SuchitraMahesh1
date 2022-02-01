df6ed968fe1abc666629eded4610e7bfbc88acab
suchitra <suchitra9049@gmail.com>
2022-02-01 16:40:10 +0530

    initial commit

---
+++ src/main/java/com/springrest/springrest/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.springrest.springrest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    String getHelloWorld(){
+        return "<h1>My first web page</h1>";
+    }
+}
