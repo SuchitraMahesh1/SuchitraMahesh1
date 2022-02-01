e6da035fa57d0b5753c0dbc8d4b3fe307e83c34f
suchitra <suchitra9049@gmail.com>
2022-02-01 18:04:57 +0530

    Add test

--- src/test/java/com/springrest/springrest/SpringrestApplicationTests.java
+++ src/test/java/com/springrest/springrest/SpringrestApplicationTests.java
@@ -1,12 +1,16 @@
 package com.springrest.springrest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @SpringBootTest
 class SpringrestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(false);
 	}
 }
